e094353bdeba0a06a1c1fe8e142e74e27266310f
seony42 <seony42@nasmedia.co.kr>
2026-03-25 13:55:01 +0900

    release: AdMixerMediationGAM 1.0.6

--- Package.swift
+++ Package.swift
@@ -25,8 +25,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdMixerMediationGAMBinary",
-            url: "https://github.com/Nasmedia-Tech/iOS-AdMixerDownload/raw/main/AdMixerMediationGAM1.0.6-beta.xcframework.zip",
-            checksum: "3d91fabe407212c9b801b1938651b014f67c4213533fb0855a009b7c9e0ac48d"
+            url: "https://github.com/Nasmedia-Tech/iOS-AdMixerDownload/raw/main/AdMixerMediationGAM1.0.6.xcframework.zip",
+            checksum: "29f218daf52ec1e20ed881b2aeef14e2d02c981cc85bbea9d4ecb6d514f04515"
         ),
         .target(
             name: "iOS_SSP_GAM_SPM",
